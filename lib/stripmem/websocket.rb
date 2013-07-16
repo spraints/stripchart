1421b87058ef67916cb979af07f451f89808f394
Matt Burke <spraints@gmail.com>
2013-07-16 15:55:27 -0400

    Send all data on client connection.

--- lib/stripmem/websocket.rb
+++ lib/stripmem/websocket.rb
@@ -5,11 +5,14 @@ module StripMem
   class WebSocket
     def initialize(channel)
       @channel = channel
+      @data = []
+      @channel.subscribe { |msg| @data << msg }
     end
 
     def run!
       EventMachine::WebSocket.start(:host => 'localhost', :port => 9998) do |ws|
         ws.onopen do
+          @data.each { |msg| ws.send(JSON.generate(msg)) }
           sid = @channel.subscribe { |msg| ws.send(JSON.generate(msg)) }
           ws.onclose { @channel.unsubscribe(sid) }
         end
